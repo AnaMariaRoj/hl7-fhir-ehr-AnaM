e4d7a6b1b90c4b033ed18956d25dbbcaa66207e8
AnaMariaRoj <rojasgalvisanamaria@gmail.com>
2025-03-12 17:06:18 -0500

    Actualización de mi contraseña

--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 
 
 def connect_to_mongodb(db_name, collection_name):
-    uri = "mongodb+srv://rojasgalvisanamaria:<Nicolas2003.>@anamariarojas.05rny.mongodb.net/?retryWrites=true&w=majority&appName=AnaMariaRojas"
+    uri = "mongodb+srv://rojasgalvisanamaria:<Nicolas2005.>@anamariarojas.05rny.mongodb.net/?retryWrites=true&w=majority&appName=AnaMariaRojas"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
