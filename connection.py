ad29467ef9e55ef73ae4103e4585fdac87a77128
AnaMariaRoj <rojasgalvisanamaria@gmail.com>
2025-03-18 18:05:55 -0500

    Conectar a mi base de datos2

--- connection.py
+++ connection.py
@@ -3,7 +3,7 @@ from pymongo.server_api import ServerApi
 
 
 def connect_to_mongodb(db_name, collection_name):
-    uri = "mongodb+srv://rojasgalvisanamaria:<Nicolas2005.>@anamariarojas.05rny.mongodb.net/?retryWrites=true&w=majority&appName=AnaMariaRojas"
+    uri = "mongodb+srv://anarojasg:<Nicolas2003.>@cluster0.drwws.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
