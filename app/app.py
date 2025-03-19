35a9e1eac79d5ba7d9a9a2c1237049b0c2c6139d
AnaMariaRoj <rojasgalvisanamaria@gmail.com>
2025-03-19 17:13:10 -0500

    Update app.py Final

--- app/app.py
+++ app/app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://hl7-fhir-ehr-anam-0132.onrender.com"],  # Permitir solo este dominio
+    allow_origins=["*"],  # Permitir solo este dominio
     allow_credentials=True,
     allow_methods=["*"],  # Permitir todos los métodos (GET, POST, etc.)
     allow_headers=["*"],  # Permitir todos los encabezados
