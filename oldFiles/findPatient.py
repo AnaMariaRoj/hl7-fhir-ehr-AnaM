ae7227313f6f80c2e3feca82ddb68eec9e5cc2df
AnaMariaRoj <rojasgalvisanamaria@gmail.com>
2025-04-01 22:54:16 +0000

    Pasarlo a HIS

--- oldFiles/findPatient.py
+++ oldFiles/findPatient.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     uri = "mongodb+srv://mardugo:clave@sampleinformationservic.t2yog.mongodb.net/?retryWrites=true&w=majority&appName=SampleInformationService"
 
     # Nombre de la base de datos y la colección
-    db_name = "SamplePatientService"
+    db_name = "HIS"
     collection_name = "patients"
 
     # Conectar a MongoDB
